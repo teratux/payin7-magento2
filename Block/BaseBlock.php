b3b083a7d3d0363ce425a5253c0feeaeef6c3108
Roberto Espi <teratux@gmail.com>
2021-09-13 22:36:38 -0400

    Round other price block

--- Block/BaseBlock.php
+++ Block/BaseBlock.php
@@ -26,7 +26,7 @@ class BaseBlock extends \Magento\Framework\View\Element\Template {
         if(!empty($commissions[0]['installments'])) {
             foreach($commissions[0]['installments'] as $installment) {
                 if($installment['months'] > $months) {
-                    $price = $installment['amount'];
+                    $price = round($installment['amount'], 0);
                     $months = $installment['months'];
                 }
             }
